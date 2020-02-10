0a3e469c26e12b1b3b53ee24e5140c7b8a4ad330
Tia Hwang <tiajshwang@gmail.com>
2020-02-10 21:43:24 +0900

    Added Sticky bar and Anchor page move

--- js/main.js
+++ js/main.js
@@ -2,11 +2,25 @@ const toggle = document.querySelector(".toggle");
 const toggleBtn = document.getElementById("toggle-btn");
 
 toggleBtn.addEventListener("click", function() {
-  toggle.classList.toggle("on");
+    toggle.classList.toggle("on");
 });
 
 window.addEventListener("resize", function() {
-  if (window.innerWidth > 768) {
-    toggle.classList.remove("on");
-  }
+    if (window.innerWidth > 768) {
+        toggle.classList.remove("on");
+    }
 });
+
+// sticky nav bar
+window.onscroll = function() {
+    const navbar = document.querySelector(".header__nav");
+
+    if (window.pageYOffset >= 780) {
+        navbar.classList.add("sticky");
+        toggleBtn.addEventListener("click", () => {
+            navbar.classList.toggle("active");
+        });
+    } else {
+        navbar.classList.remove("sticky");
+    }
+};
